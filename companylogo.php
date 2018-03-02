42911faefdcbcc9d1fa28940bf673170402e8931
yagmurguruf <36995295+yagmurguruf@users.noreply.github.com>
2018-03-02 18:57:18 +0300

    Create companylogo.php

---
+++ companylogo.php
@@ -0,0 +1,325 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: w10
+ * Date: 2.02.2018
+ * Time: 14:42
+ */
+
+namespace Core\Utility;
+
+
+class CompanyLogo
+{
+    static function getCompanyLogo($name) {
+
+        switch ($name) {
+           /* case 1072:
+                return '<img class="company_logo" src ="/igsicompanylogo/1072.png">'; */
+            case 1551:
+                return '<img class="company_logo" src ="/igsicompanylogo/1551.png">';
+            case 1493:
+                return '<img class="company_logo" src ="/igsicompanylogo/1493.png">';
+            case 1276:
+                return '<img class="company_logo" src="/igsicompanylogo/1276.png">';
+            case 1072:
+                return '<img class="company_logo" src="/igsicompanylogo/1072.png">';
+            case 1521:
+                return '<img class="company_logo" src="/igsicompanylogo/1521.png">';
+            case 1460:
+                return '<img class="company_logo" src="/igsicompanylogo/1460.png">';
+            case 1432:
+                return '<img class="company_logo" src="/igsicompanylogo/1432.png">';
+            case 1515:
+                return '<img class="company_logo" src="/igsicompanylogo/1515.png">';
+            case 1268:
+                return '<img class="company_logo" src="/igsicompanylogo/1268.png">';
+            case 1190:
+                return '<img class="company_logo" src="/igsicompanylogo/1190.png">';
+            case 1357:
+                return '<img class="company_logo" src="/igsicompanylogo/1357.png">';
+            case 1382:
+                return '<img class="company_logo" src="/igsicompanylogo/1382.png">';
+            case 1533:
+                return '<img class="company_logo" src="/igsicompanylogo/1533.png">';
+            case 1417:
+                return '<img class="company_logo" src="/igsicompanylogo/1417.png">';
+            case 1523:
+                return '<img class="company_logo" src="/igsicompanylogo/1523.png">';
+            case 1498:
+                return '<img class="company_logo" src="/igsicompanylogo/1498.png">';
+            case 1565:
+                return '<img class="company_logo" src="/igsicompanylogo/1565.png">';
+            case 1419:
+                return '<img class="company_logo" src="/igsicompanylogo/1419.png">';
+            case 1344:
+                return '<img class="company_logo" src="/igsicompanylogo/1344.png">';
+            case 1321:
+                return '<img class="company_logo" src="/igsicompanylogo/1321.png">';
+            case 1088:
+                return '<img class="company_logo" src="/igsicompanylogo/1088.png">';
+            case 1509:
+                return '<img class="company_logo" src="/igsicompanylogo/1509.png">';
+            case 1232:
+                return '<img class="company_logo" src="/igsicompanylogo/1232.png">';
+            case 1566:
+                return '<img class="company_logo" src="/igsicompanylogo/1566.png">';
+            case 1314:
+                return '<img class="company_logo" src="/igsicompanylogo/1314.png">';
+            case 1422:
+                return '<img class="company_logo" src="/igsicompanylogo/1422.png">';
+            case 1571:
+                return '<img class="company_logo" src="/igsicompanylogo/1571.png">';
+            case 1339:
+                return '<img class="company_logo" src="/igsicompanylogo/1339.png">';
+            case 1370:
+                return '<img class="company_logo" src="/igsicompanylogo/1370.png">';
+            case 1555:
+                return '<img class="company_logo" src="/igsicompanylogo/1555.png">';
+            case 1258:
+                return '<img class="company_logo" src="/igsicompanylogo/1258.png">';
+            case 1148:
+                return '<img class="company_logo" src="/igsicompanylogo/1148.png">';
+            case 1092:
+                return '<img class="company_logo" src="/igsicompanylogo/1092.png">';
+            case 1550:
+                return '<img class="company_logo" src="/igsicompanylogo/1550.png">';
+            case 1524:
+                return '<img class="company_logo" src="/igsicompanylogo/1524.png">';
+            case 1529:
+                return '<img class="company_logo" src="/igsicompanylogo/1529.png">';
+            case 1093:
+                return '<img class="company_logo" src="/igsicompanylogo/1093.png">';
+            case 1559:
+                return '<img class="company_logo" src="/igsicompanylogo/1559.png">';
+            case 1315:
+                return '<img class="company_logo" src="/igsicompanylogo/1315.png">';
+            case 1453:
+                return '<img class="company_logo" src="/igsicompanylogo/1453.png">';
+            case 1399:
+                return '<img class="company_logo" src="/igsicompanylogo/1399.png">';
+            case 1574:
+                return '<img class="company_logo" src="/igsicompanylogo/1574.png">';
+            case 1396:
+                return '<img class="company_logo" src="/igsicompanylogo/1396.png">';
+            case 1426:
+                return '<img class="company_logo" src="/igsicompanylogo/1426.png">';
+            case 1437:
+                return '<img class="company_logo" src="/igsicompanylogo/1437.png">';
+            case 1375:
+                return '<img class="company_logo" src="/igsicompanylogo/1375.png">';
+            case 1273:
+                return '<img class="company_logo" src="/igsicompanylogo/1273.png">';
+            case 1530:
+                return '<img class="company_logo" src="/igsicompanylogo/1530.png">';
+            case 1141:
+                return '<img class="company_logo" src="/igsicompanylogo/1141.png">';
+            case 1286:
+                return '<img class="company_logo" src="/igsicompanylogo/1286.png">';
+            case 1478:
+                return '<img class="company_logo" src="/igsicompanylogo/1478.png">';
+            case 1119:
+                return '<img class="company_logo" src="/igsicompanylogo/1119.png">';
+            case 1392:
+                return '<img class="company_logo" src="/igsicompanylogo/1392.png">';
+            case 1433:
+                return '<img class="company_logo" src="/igsicompanylogo/1433.png">';
+            case 1184:
+                return '<img class="company_logo" src="/igsicompanylogo/1184.png">';
+            case 1372:
+                return '<img class="company_logo" src="/igsicompanylogo/1372.png">';
+            case 1394:
+                return '<img class="company_logo" src="/igsicompanylogo/1394.png">';
+            case 1430:
+                return '<img class="company_logo" src="/igsicompanylogo/1430.png">';
+            case 1103:
+                return '<img class="company_logo" src="/igsicompanylogo/1103.png">';
+            case 1484:
+                return '<img class="company_logo" src="/igsicompanylogo/1484.png">';
+            case 1431:
+                return '<img class="company_logo" src="/igsicompanylogo/1431.png">';
+            case 1384:
+                return '<img class="company_logo" src="/igsicompanylogo/1384.png">';
+            case 1295:
+                return '<img class="company_logo" src="/igsicompanylogo/1295.png">';
+            case 1579:
+                return '<img class="company_logo" src="/igsicompanylogo/1579.png">';
+            case 1556:
+                return '<img class="company_logo" src="/igsicompanylogo/1556.png">';
+            case 1487:
+                return '<img class="company_logo" src="/igsicompanylogo/1487.png">';
+            case 1424:
+                return '<img class="company_logo" src="/igsicompanylogo/1424.png">';
+            case 1488:
+                return '<img class="company_logo" src="/igsicompanylogo/1488.png">';
+            case 1544:
+                return '<img class="company_logo" src="/igsicompanylogo/1544.png">';
+            case 1342:
+                return '<img class="company_logo" src="/igsicompanylogo/1342.png">';
+            case 1387:
+                return '<img class="company_logo" src="/igsicompanylogo/1387.png">';
+            case 1284:
+                return '<img class="company_logo" src="/igsicompanylogo/1284.png">';
+            case 1582:
+                return '<img class="company_logo" src="/igsicompanylogo/1582.png">';
+            case 1464:
+                return '<img class="company_logo" src="/igsicompanylogo/1464.png">';
+            case 1485:
+                return '<img class="company_logo" src="/igsicompanylogo/1485.png">';
+            case 1250:
+                return '<img class="company_logo" src="/igsicompanylogo/1250.png">';
+            case 1398:
+                return '<img class="company_logo" src="/igsicompanylogo/1398.png">';
+            case 1306:
+                return '<img class="company_logo" src="/igsicompanylogo/1306.png">';
+            case 1393:
+                return '<img class="company_logo" src="/igsicompanylogo/1393.png">';
+            case 1395:
+                return '<img class="company_logo" src="/igsicompanylogo/1395.png">';
+            case 1534:
+                return '<img class="company_logo" src="/igsicompanylogo/1534.png">';
+            case 1259:
+                return '<img class="company_logo" src="/igsicompanylogo/1259.png">';
+            case 1581:
+                return '<img class="company_logo" src="/igsicompanylogo/1581.png">';
+            case 1511:
+                return '<img class="company_logo" src="/igsicompanylogo/1511.png">';
+            case 1562:
+                return '<img class="company_logo" src="/igsicompanylogo/1562.png">';
+            case 1234:
+                return '<img class="company_logo" src="/igsicompanylogo/1234.png">';
+            case 1301:
+                return '<img class="company_logo" src="/igsicompanylogo/1301.png">';
+            case 1440:
+                return '<img class="company_logo" src="/igsicompanylogo/1440.png">';
+            case 1311:
+                return '<img class="company_logo" src="/igsicompanylogo/1311.png">';
+            case 1423:
+                return '<img class="company_logo" src="/igsicompanylogo/1423.png">';
+            case 1434:
+                return '<img class="company_logo" src="/igsicompanylogo/1434.png">';
+            case 1474:
+                return '<img class="company_logo" src="/igsicompanylogo/1474.png">';
+            case 1573:
+                return '<img class="company_logo" src="/igsicompanylogo/1573.png">';
+            case 1572:
+                return '<img class="company_logo" src="/igsicompanylogo/1572.png">';
+            case 1526:
+                return '<img class="company_logo" src="/igsicompanylogo/1526.png">';
+            case 1525:
+                return '<img class="company_logo" src="/igsicompanylogo/1525.png">';
+            case 1496:
+                return '<img class="company_logo" src="/igsicompanylogo/1496.png">';
+            case 1405:
+                return '<img class="company_logo" src="/igsicompanylogo/1405.png">';
+            case 1334:
+                return '<img class="company_logo" src="/igsicompanylogo/1334.png">';
+            case 1508:
+                return '<img class="company_logo" src="/igsicompanylogo/1508.png">';
+            case 1501:
+                return '<img class="company_logo" src="/igsicompanylogo/1501.png">';
+            case 1497:
+                return '<img class="company_logo" src="/igsicompanylogo/1497.png">';
+             case 1347:
+                return '<img class="company_logo" src="/igsicompanylogo/1347.png">';
+            case 1235:
+                return '<img class="company_logo" src="/igsicompanylogo/1235.png">';
+            case 1528:
+                return '<img class="company_logo" src="/igsicompanylogo/1528.png">';
+            case 1495:
+                return '<img class="company_logo" src="/igsicompanylogo/1495.png">';
+            case 1454:
+                return '<img class="company_logo" src="/igsicompanylogo/1454.png">';
+            case 1584:
+                return '<img class="company_logo" src="/igsicompanylogo/1584.png">';
+            case 1123:
+                return '<img class="company_logo" src="/igsicompanylogo/1123.png">';
+            case 1507:
+                return '<img class="company_logo" src="/igsicompanylogo/1507.png">';
+            case 1361:
+                return '<img class="company_logo" src="/igsicompanylogo/1361.png">';
+            case 1561:
+                return '<img class="company_logo" src="/igsicompanylogo/1561.png">';
+            case 1335:
+                return '<img class="company_logo" src="/igsicompanylogo/1335.png">';
+            case 1160:
+                return '<img class="company_logo" src="/igsicompanylogo/1160.png">';
+            case 1338:
+                return '<img class="company_logo" src="/igsicompanylogo/1338.png">';
+            case 1465:
+                return '<img class="company_logo" src="/igsicompanylogo/1465.png">';
+            case 1366:
+                return '<img class="company_logo" src="/igsicompanylogo/1366.png">';
+            case 1368:
+                return '<img class="company_logo" src="/igsicompanylogo/1368.png">';
+            case 1125:
+                return '<img class="company_logo" src="/igsicompanylogo/1125.png">';
+            case 1317:
+                return '<img class="company_logo" src="/igsicompanylogo/1317.png">';
+            case 1421:
+                return '<img class="company_logo" src="/igsicompanylogo/1421.png">';
+            case 1121:
+                return '<img class="company_logo" src="/igsicompanylogo/1121.png">';
+            case 1541:
+                return '<img class="company_logo" src="/igsicompanylogo/1541.png">';
+            case 1139:
+                return '<img class="company_logo" src="/igsicompanylogo/1139.png">';
+            case 1542:
+                return '<img class="company_logo" src="/igsicompanylogo/1542.png">';
+            case 1371:
+                return '<img class="company_logo" src="/igsicompanylogo/1371.png">';
+            case 1254:
+                return '<img class="company_logo" src="/igsicompanylogo/1254.png">';
+            case 1415:
+                return '<img class="company_logo" src="/igsicompanylogo/1415.png">';
+            case 1486:
+                return '<img class="company_logo" src="/igsicompanylogo/1486.png">';
+            case 1391:
+                return '<img class="company_logo" src="/igsicompanylogo/1391.png">';
+            case 1379:
+                return '<img class="company_logo" src="/igsicompanylogo/1379.png">';
+            case 1470:
+                return '<img class="company_logo" src="/igsicompanylogo/1470.png">';
+            case 1340:
+                return '<img class="company_logo" src="/igsicompanylogo/1340.png">';
+            case 1260:
+                return '<img class="company_logo" src="/igsicompanylogo/1260.png">';
+            case 1557:
+                return '<img class="company_logo" src="/igsicompanylogo/1557.png">';
+            case 1580:
+                return '<img class="company_logo" src="/igsicompanylogo/1580.png">';
+            case 1348:
+                return '<img class="company_logo" src="/igsicompanylogo/1348.png">';
+            case 1133:
+                return '<img class="company_logo" src="/igsicompanylogo/1133.png">';
+            case 1328:
+                return '<img class="company_logo" src="/igsicompanylogo/1328.png">';
+            case 1270:
+                return '<img class="company_logo" src="/igsicompanylogo/1270.png">';
+            case 1512:
+                return '<img class="company_logo" src="/igsicompanylogo/1512.png">';
+            case 1482:
+                return '<img class="company_logo" src="/igsicompanylogo/1482.png">';
+            case 1354:
+                return '<img class="company_logo" src="/igsicompanylogo/1354.png">';
+            case 1538:
+                return '<img class="company_logo" src="/igsicompanylogo/1538.png">';
+            case 1504:
+                return '<img class="company_logo" src="/igsicompanylogo/1504.png">';
+            case 1583:
+                return '<img class="company_logo" src="/igsicompanylogo/1583.png">';
+            case 1503:
+                return '<img class="company_logo" src="/igsicompanylogo/1503.png">';
+            case 1576:
+                return '<img class="company_logo" src="/igsicompanylogo/1576.png">';
+            case  1554:
+                return '<img class="company_logo" src="/igsicompanylogo/1554.png">';
+            case  1281:
+                return '<img class="company_logo" src="/igsicompanylogo/1281.png">';
+
+                default:
+                return "";
+        }
+
+    }
+}
